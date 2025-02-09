06783adf6c23cfe6491674cbd2785194f3413bd2
Oleg <tester@intelex.ca>
2025-02-09 23:24:02 +0200

    Add Payments (Payment, PaymentGroup, UserToPaymentGroup) entities

---
+++ src/back-end/WhereIsMyMoney.DAL/Entities/Payment/PaymentGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhereIsMyMoney.DAL.Entities.Abstract;
+
+namespace WhereIsMyMoney.DAL.Entities.Payment
+{
+    public record class PaymentGroup : EntityBase
+    {
+        public required string Name {  get; set; }
+        public required int CurrencyId { get; set; }
+
+    }
+}
