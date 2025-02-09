26b8dcfe7fbbb710488d0935b58e510478d92cd1
Oleg <tester@intelex.ca>
2025-02-09 23:20:44 +0200

    Add User entity

---
+++ src/back-end/WhereIsMyMoney.DAL/Entities/User/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhereIsMyMoney.DAL.Entities.Abstract;
+
+namespace WhereIsMyMoney.DAL.Entities.User
+{
+    public record class User : EntityBase
+    {
+        [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$")]
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        
+        [StringLength(50)]
+        public required string FirstName { get; set; }
+        
+        [StringLength(50)] 
+        public required string LastName { get; set; }
+
+        public string FullName
+        {
+            get => $"{FirstName} {LastName}";
+        }
+
+    }
+}
