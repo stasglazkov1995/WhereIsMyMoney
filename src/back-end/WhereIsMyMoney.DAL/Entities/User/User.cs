e7c899ce95c28e9a231c8f0e8c006cf9f91ca5c8
Oleg <tester@intelex.ca>
2025-02-10 21:55:41 +0200

    Add required fields to User entity

--- src/back-end/WhereIsMyMoney.DAL/Entities/User/User.cs
+++ src/back-end/WhereIsMyMoney.DAL/Entities/User/User.cs
@@ -1,22 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 using WhereIsMyMoney.DAL.Entities.Abstract;
 
 namespace WhereIsMyMoney.DAL.Entities.User
 {
     public record class User : EntityBase
     {
+        [Required]
         [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$")]
         public required string Email { get; set; }
+
         public required string Password { get; set; }
-        
+
+        [Required]
         [StringLength(50)]
         public required string FirstName { get; set; }
-        
+
+        [Required]
         [StringLength(50)] 
         public required string LastName { get; set; }
 
