7cca70397792958c060cc569d8961a3e90f03316
Oleg <tester@intelex.ca>
2025-02-10 22:17:06 +0200

    Add Currency etity

---
+++ src/back-end/WhereIsMyMoney.DAL/Entities/Payment/Currency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhereIsMyMoney.DAL.Entities.Payment
+{
+    public class Currency
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Code { get; set; }
+    }
+}
