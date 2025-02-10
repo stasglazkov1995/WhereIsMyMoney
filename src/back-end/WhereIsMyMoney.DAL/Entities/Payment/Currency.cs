c40130484af0f51eb2665404895e9362ec606c4c
Oleg <tester@intelex.ca>
2025-02-10 22:25:57 +0200

    Fix id

--- src/back-end/WhereIsMyMoney.DAL/Entities/Payment/Currency.cs
+++ src/back-end/WhereIsMyMoney.DAL/Entities/Payment/Currency.cs
@@ -8,7 +8,7 @@ namespace WhereIsMyMoney.DAL.Entities.Payment
 {
     public class Currency
     {
-        public int Id { get; set; }
+        public int Id { get; init; }
         public required string Name { get; set; }
         public required string Code { get; set; }
     }
