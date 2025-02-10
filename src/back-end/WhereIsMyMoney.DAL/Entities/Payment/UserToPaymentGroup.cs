6f5f3dfb63fe04b0d1ac13f395c633e4aba3c7ba
Oleg <tester@intelex.ca>
2025-02-10 22:22:50 +0200

    Add User to Group entity

--- src/back-end/WhereIsMyMoney.DAL/Entities/Payment/UserToPaymentGroup.cs
+++ src/back-end/WhereIsMyMoney.DAL/Entities/Payment/UserToPaymentGroup.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace WhereIsMyMoney.DAL.Entities.Payment
 {
-    internal class UserToPaymentGroup
+    public class UserToPaymentGroup
     {
+        public int Id { get; init; }
+        public required int UserId { get; set; }
+        public required int GroupId { get; set; }
+
     }
 }
