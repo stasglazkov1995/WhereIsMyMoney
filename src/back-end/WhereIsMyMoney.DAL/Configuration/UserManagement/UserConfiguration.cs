3ba38f5ca1cbaff9674ec60768ef9fa8e407fcba
Stanislav Hlazkov <stas.glazkov95@gmail.com>
2025-02-11 21:56:01 +0100

    Added User entity, configuration user entity, Users DbSet to DbContext

---
+++ src/back-end/WhereIsMyMoney.DAL/Configuration/UserManagement/UserConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WhereIsMyMoney.DAL.Configuration.Abstract;
+using WhereIsMyMoney.DAL.Entities.UserManagement;
+
+namespace WhereIsMyMoney.DAL.Configuration.UserManagement;
+
+public class UserConfiguration : BaseEntityTypeConfiguration<User>
+{
+    public override void Configure(EntityTypeBuilder<User> modelBuilder)
+    {
+        base.Configure(modelBuilder);
+        modelBuilder.Ignore(x => x.FullName);
+            
+        modelBuilder.Property(x => x.FirstName)
+            .HasMaxLength(100)
+            .IsRequired();
+        modelBuilder.Property(x => x.LastName)
+            .HasMaxLength(200)
+            .IsRequired();
+        modelBuilder.Property(x => x.Email)
+            .HasMaxLength(255)
+            .IsRequired();
+        modelBuilder.Property(x => x.Password)
+            .HasMaxLength(255)
+            .IsRequired();
+    }
+}
